ebcde06dad4af9a76a537e56237102467f95f7bf
AnnaGoncharova12 <agoncharov@brynmawr.edu>
2021-09-09 15:01:44 -0400

    intermediate commit(no readme)

--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -9,13 +9,26 @@ class Sphere2D : public atkui::Framework {
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
-    setColor(vec3(0,1,0));
+    
 
     // draw a sphere at center of screen
     double x = width() * 0.5;
     double y = height() * 0.5;
+    /*
+    double x1=width() * 0.75;
+    double y1=height() * 0.75;
+    */
     double radius = 100; 
-    drawSphere(vec3(x,y,0), radius);
+
+   
+       
+    setColor(vec3(0,1,0));
+    drawSphere(vec3(x,y,100), radius);
+      
+    setColor(vec3(1,0,0));
+    drawSphere(vec3(x,y,0), radius*2);
+ 
+    
   }
 };
 
--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -6,15 +6,31 @@ class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
   }
+  virtual void setup(){
+    currentPos= vec3(0, 0, 0);
+    velocity = vec3(0, 0, 0);
+  }
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
-
+    currentPos = currentPos + velocity * dt();
     // draw a sphere at center of the world
     float radius = 50.0;
-    drawSphere(vec3(0), radius);
+    drawSphere(currentPos, radius);
   }
+  virtual void keyUp(int key, int mods){
+     if(key == GLFW_KEY_SPACE){
+        velocity=25.0f * agl::randomUnitVector();
+     }
+     else if(key == GLFW_KEY_R){
+        currentPos= vec3(0, 0, 0);
+        velocity = vec3(0, 0, 0);
+     }
+  }
+  private:
+   vec3 currentPos;
+   vec3 velocity;
 };
 
 int main(int argc, char** argv)
