5d7a9726276a8e95b3da1a2f2a7104d7ca4e4b2d
AnnaGoncharova12 <agoncharov@brynmawr.edu>
2021-09-24 23:24:04 -0400

    final

--- assignments/a2-interpolation/unique.cpp
+++ assignments/a2-interpolation/unique.cpp
@@ -23,19 +23,40 @@ class Unique : public atkui::Framework {
   void scene() {
 
      //we want range for 2nd cpt[60*(i+1)-60, 60*(i+1)+60 ]
-     for(int j=0;j<7;j++){
-        setColor(colors[j]);
-      vec3 currPos=cpts[j][0];
+      for(int j=0;j<7;j++){
+       
       cpts[j][1]=vec3(60*(j+1)-60*sin(elapsedTime()*2), 110, 0);
       cpts[j][2]=vec3(60*(j+1)+60*sin(elapsedTime()*2), 110, 0);
+      }
+     for(int j=0;j<6;j++){
+        setColor(colors[j]);
+      vec3 currPos=cpts[j][0];
+      //cpts[j][1]=vec3(60*(j+1)-60*sin(elapsedTime()*2), 110, 0);
+      //cpts[j][2]=vec3(60*(j+1)+60*sin(elapsedTime()*2), 110, 0);
       //draw curve at time t
-         for(float i = 0;i<=1;i+=0.01){
-            vec3 p=((float)pow(1-i, 3.0))*cpts[j][0]+3*i*((float)pow(1-i, 2.0))*cpts[j][1]+3*((float)pow(i, 2.0))*(1-i)*cpts[j][2]+((float)pow(i, 3.0))*cpts[j][3];
+      vec3 arr[4]={};
+      for(int k=0;k<4;k++){
+         arr[k]=cpts[j][k];
+      }
+      float t=0;
+         while(t<1&&arr[1].x<cpts[j+1][1].x&&arr[2].x<cpts[j+1][2].x){
+            for(float i = 0;i<=1;i+=0.01){
+               vec3 p=((float)pow(1-i, 3.0))*arr[0]+3*i*((float)pow(1-i, 2.0))*arr[1]+3*((float)pow(i, 2.0))*(1-i)*arr[2]+((float)pow(i, 3.0))*arr[3];
           
-            drawLine(currPos, p);
-            currPos=p;
+               drawLine(currPos, p);
+               currPos=p;
+         }
+         t+=0.1;
+         //lerp between different color sectors
+         for(int k=0;k<4;k++){
+         arr[k]=LERP(cpts[j][k], cpts[j+1][k], t);
+       }
       }
      }
+  }
+  vec3 LERP(vec3 p1, vec3 p2, float t){
+   vec3 res=p1*(1-t)+p2*t;
+   return res;
   }
   private:
   vec3 colors [7]={
